a06f5bdaa0617ba63d61740a55ac426605f18700
h1divp <71522316+h1divp@users.noreply.github.com>
2025-04-08 16:17:53 -0400

    improved crud operations and editing page UX

--- client/app/editor/page.tsx
+++ client/app/editor/page.tsx
@@ -4,31 +4,14 @@ import { createClient } from '@supabase/supabase-js';
 import { useRouter } from 'next/navigation';
 import { Button } from "@heroui/button";
 import { NavigationBar } from '@/components/Navbar';
-import { Textarea } from '@heroui/input';
+import { Input, Textarea } from '@heroui/input';
+import { groupDataFocusVisibleClasses } from '@heroui/theme';
+import { Database } from '@/types/database.types';
+import { useUserContext } from '@/contexts/UserProvider';
 
 const Editor: React.FC = () => {
   const router = useRouter();
-
-  // State for managing deck creation
-  const [deckName, setDeckName] = useState<string>('');
-  const [currentFront, setCurrentFront] = useState<string>('');
-  const [currentBack, setCurrentBack] = useState<string>('');
-
-  // State to track front and back texts before saving
-  const [frontTexts, setFrontTexts] = useState<string[]>([]);
-  const [backTexts, setBackTexts] = useState<string[]>([]);
-
-  // Add a card to the current deck
-  // const handleAddCard = () => {
-  //   if (currentFront.trim() && currentBack.trim()) {
-  //     setFrontTexts([...frontTexts, currentFront]);
-  //     setBackTexts([...backTexts, currentBack]);
-
-  //     // Clear input fields
-  //     setCurrentFront('');
-  //     setCurrentBack('');
-  //   }
-  // };
+  const session = useUserContext();
 
   // Save the entire deck
   // const handleSaveDeck = async () => {
@@ -62,24 +45,59 @@ const Editor: React.FC = () => {
   //   }
   // };
 
-  const [deck, setDeck] = useState<{ front: string, back: string }[] | undefined>(undefined);
+  type Card = { front: string, back: string }
+
+  const [deck, setDeck] = useState<Card[]>([]);
   const [deckId, setDeckId] = useState<string>("");
+  const [deckName, setDeckName] = useState<string>("New Deck");
+  const [currentCard, setCurrentCard] = useState<Card>({ front: "", back: "" });
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [isNewDeck, setIsNewDeck] = useState<boolean>(true);
   const lorem = "Labore commodi est et sit sint accusamus. Sed laudantium impedit similique quis cumque a et. Nulla sit ut impedit itaque cupiditate quasi doloribus. Perspiciatis dolores nulla saepe occaecati exercitationem consequuntur. Dignissimos iusto nihil aut. Aut sit pariatur officiis qui reiciendis aut officiis et.";
 
-  // Remove a card from the current deck
-  // const handleRemoveCard = (index: number) => {
-  //   const newFrontTexts = [...frontTexts];
-  //   const newBackTexts = [...backTexts];
+  // 1. If passed a deck id, load the deck; if not, fill in inputs for a new deck
+  // 2. If passed a deck id, db query deck and load into local state, render an error warning if error
+  // Update deck name col when name save button is pressed
+  // Implement edit buttons
+  // Implement delete buttons
+  // Update db row when deck save button is pressed
 
-  //   newFrontTexts.splice(index, 1);
-  //   newBackTexts.splice(index, 1);
+  const handleAddCard = () => {
+    setDeck([...deck, currentCard]);
+    setCurrentCard({ front: "", back: "" })
+    console.log(deck)
+  }
 
-  //   setFrontTexts(newFrontTexts);
-  //   setBackTexts(newBackTexts);
-  // };
+  const handleDeleteCard = (index: number) => {
+    if (index < 0 || index >= deck.length) return;
+    deck.splice(index, 1);
+    setDeck([...deck])
+  }
+
+  const handleEditCard = () => {
+    setIsEditing(!isEditing);
+  }
+
+  const handleClearNewCard = () => {
+    setCurrentCard({ front: "", back: "" })
+  }
+
+  const handleSaveDeck = () => {
+    setIsEditing(!isEditing);
+  }
+
+  const handleCreateDeck = () => {
+    setIsNewDeck(!isNewDeck);
+  }
+
+  const handleDeleteDeck = () => {
+    setDeck([]);
+    setDeckName("New Deck");
+    setIsEditing(false);
+    setIsNewDeck(true);
+  }
 
   useEffect(() => {
-
   }, [])
 
   return (
@@ -88,83 +106,97 @@ const Editor: React.FC = () => {
 
       <div className="container mx-auto flex-1">
         {/*TODO: pull text data from loaded deck obj*/}
-        <div className='flex flex-row'>
-          <h1 className="text-2xl font-bold mb-4">New Deck</h1>
-          <Button variant='ghost' size='sm' className='ml-2'>Edit Name</Button>
-          <Button variant='ghost' size='sm' className='ml-2'>Save</Button>
-        </div>
+        <h1 className="text-2xl font-bold mb-4">Create Deck</h1>
 
         <div className="grid grid-cols-2 gap-6">
           <Textarea
             label="New card front"
-            value={currentFront}
-            onChange={(e) => setCurrentFront(e.target.value)}
+            value={currentCard.front}
+            onChange={(e) => setCurrentCard({ ...currentCard, front: e.target.value })}
             className="w-full"
             labelPlacement="outside"
+            variant='bordered'
           />
           <Textarea
             label="New card back"
-            value={currentBack}
-            onChange={(e) => setCurrentBack(e.target.value)}
+            value={currentCard.back}
+            onChange={(e) => setCurrentCard({ ...currentCard, back: e.target.value })}
             className="w-full"
             labelPlacement="outside"
+            variant='bordered'
           />
         </div>
-        <Button variant='ghost' size='sm' className='mt-2'>Add</Button>
-        <Button variant='ghost' size='sm' className='mt-2 ml-2'>Clear</Button>
-        <hr className='my-6' />
+        <div className='flex flex-row mt-2 gap-2'>
+          <Button variant='ghost' size='sm' onPress={() => handleAddCard()}>Add Card</Button>
+          <Button variant='ghost' size='sm' onPress={() => handleClearNewCard()}>Clear</Button>
+        </div>
 
-        {/* Card Preview */}
-        <h1 className="font-bold mb-4">Added cards</h1>
-        <div className='flex flex-col'>
-          <div className="container mx-auto">
-            <div className='container flex flex-row gap-6'>
-              <div className='flex flex-col justify-center'>
-                <p className='text-sm text-center'>1</p>
-                <Button variant='ghost' size='sm' className='mt-2'>Edit</Button>
-                <Button variant='ghost' size='sm' className='mt-2'>Delete</Button>
-              </div>
-              <Textarea
-                label="Front"
-                value={lorem}
-                className="w-full"
-                labelPlacement="outside"
-                isReadOnly
+        {/* Deck Preview */}
+        {deck.length == 0 ? (
+          <></>
+        ) : (
+          <>
+            <hr className='my-6' />
+            <div className='flex flex-row gap-2 mb-6 items-center'>
+              <Input
+                className='w-auto'
+                size='sm'
+                variant='bordered'
+                value={deckName}
+                onValueChange={setDeckName}
+                isClearable
               />
-              <Textarea
-                label="Back"
-                value={lorem}
-                className="w-full"
-                labelPlacement="outside"
-                isReadOnly
-              />
+              <>
+                {isNewDeck ? (
+                  <Button variant='ghost' size='sm' onPress={() => handleCreateDeck()}>Create Deck</Button>
+                ) : (
+                  <>
+                    {!isEditing ? (
+                      <Button variant='ghost' size='sm' onPress={() => handleEditCard()}>Edit Deck</Button>
+                    ) : (
+                      <>
+                        <Button variant='ghost' size='sm' onPress={() => handleSaveDeck()}>Save Deck</Button>
+                        <Button variant='ghost' size='sm' onPress={() => handleDeleteDeck()}>Delete Deck</Button>
+                      </>
+                    )}
+                  </>
+                )}
+              </>
             </div>
-          </div>
-        </div>
-        <Button variant='ghost' size='sm' className='mt-2'>Save</Button>
-
-        {/*        <div className="mt-6">
-          <h2 className="text-xl font-semibold mb-4 text-purple-600">Added cards</h2>
-          <div className="grid grid-cols-3 gap-4">
-            {frontTexts.map((front, index) => (
-              <div
-                key={index}
-                className="bg-white border border-gray-200 p-3 rounded shadow-sm relative"
-              >
-                <button
-                  onClick={() => handleRemoveCard(index)}
-                  className="absolute top-1 right-1 text-red-500 hover:text-red-700"
-                >
-                  ✕
-                </button>
-                <strong className="text-purple-600">Card {index + 1}</strong>
-                <p className="text-sm mt-2"><strong>Front:</strong> {front}</p>
-                <p className="text-sm mt-1"><strong>Back:</strong> {backTexts[index]}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-*/}      </div>
+            <div className='flex flex-col gap-2'>
+              {deck.map((card, index) => (
+                <div className="container mx-auto" key={index}>
+                  <div className='container flex flex-row gap-6 h-20'>
+                    <div className='flex flex-col justify-center gap-1'>
+                      <p className='text-sm text-center'>{index + 1}</p>
+                      <Button variant='ghost' size='sm' onPress={() => handleDeleteCard(index)}
+                        isDisabled={!isEditing}
+                      >Delete</Button>
+                    </div>
+                    <Textarea
+                      value={card.front}
+                      className="w-full"
+                      labelPlacement="outside"
+                      variant='bordered'
+                      isReadOnly
+                      isDisabled={!isEditing}
+                    />
+                    <Textarea
+                      value={card.back}
+                      className="w-full"
+                      labelPlacement="outside"
+                      variant='bordered'
+                      isReadOnly
+                      isDisabled={!isEditing}
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className='mb-6' />
+          </>
+        )}
+      </div>
     </div>
   );
 };
