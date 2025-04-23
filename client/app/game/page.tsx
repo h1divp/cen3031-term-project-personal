bd386004a51b57279667e2da0a5b81579593af94
h1divp <71522316+h1divp@users.noreply.github.com>
2025-04-22 23:46:25 -0400

    added to the game page

--- client/app/game/page.tsx
+++ client/app/game/page.tsx
@@ -1,9 +1,86 @@
 "use client";
 
+import { NavigationBar } from "@/components/Navbar";
+import { useQueryContext } from "@/contexts/QueryProvider";
+import { useUserContext } from "@/contexts/UserProvider";
+import { Tables } from "@/types/database.types";
+import { Button } from "@heroui/button";
+import { Textarea } from "@heroui/input";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+
 export default function Game() {
+  const [deck, setDeck] = useState<Tables<"decks"> | undefined>(undefined);
+  const [gameInfo, setGameInfo] = useState<Tables<"games"> | undefined>(undefined);
+  const [gameUuid, setGameUuid] = useState<string | null>(null);
+  const [cardIndex, setCardIndex] = useState<Number>(0);
+  const [showFront, setShowFront] = useState<boolean>(true);
+
+  const searchParams = useSearchParams();
+  const query = useQueryContext();
+  const session = useUserContext();
+
+  const loadGameFromUuid = async () => {
+    if (!gameUuid) return;
+    query?.getGameById(gameUuid).then((game: any) => {
+      setGameInfo(game[0]);
+
+      query?.getDeckById(game[0]["deck"]).then((deck: any) => {
+        setDeck(deck[0]);
+        console.log(deck[0]);
+      });
+
+    })
+
+  }
+
+  const handleFlip = () => {
+
+  }
+
+  useEffect(() => {
+    if (!gameUuid) return;
+    loadGameFromUuid();
+  }, [gameUuid])
+
+  useEffect(() => {
+    console.log("test")
+    const param = searchParams.get("id");
+    setGameUuid(param);
+  }, [session?.user?.id, query])
+
+  useEffect(() => {
+    session?.getSessionData();
+  })
+
+  if (!gameUuid) return (
+    <div className="min-h-screen flex flex-col">
+      <NavigationBar />
+
+      <p>There was a problem loading this deck.</p>
+
+    </div>
+  )
+
   return (
-    <div>
-      <p>Game!!!</p>
+    <div className="min-h-screen flex flex-col">
+      <NavigationBar />
+
+      <div className="flex flex-col items-center">
+        <h2 className="text-xl font-bold mb-4 text-center">{gameInfo?.name}</h2>
+
+        <div className="max-w-sm">
+          <Button
+            variant="ghost"
+            size="sm"
+            onPress={() => handleFlip()}
+          >Flip</Button>
+        </div>
+
+
+      </div>
+
     </div>
   )
 }
